c0814a0071a139b1118b4517dc3cb89ad2246bf2
lastnamearya <arya.jigyasu6815@gmail.com>
2019-04-23 04:18:28 +0530

    Adding Styled Components to our App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const Heading = styled.h1`
+  font-size: 2rem;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Heading>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Heading>
         <a
           className="App-link"
           href="https://reactjs.org"
