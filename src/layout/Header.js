f9bf0c1e6fdb6508de21556f83c17d63c3b2dbb3
lastnamearya <arya.jigyasu6815@gmail.com>
2019-07-22 22:56:56 +0530

    Scope level ~ Styling using SC

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,20 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import logo from '../logo.svg';
 
-const AppHeader = styled.header`
+const Header = ({ className }) => {
+  return (
+    <header className={className}>
+      <img src={logo} alt="logo" className="logo" />
+    </header>
+  );
+};
+
+export default styled(Header)`
   background: #524763;
   padding: 10px 5%;
   .logo {
     width: 60px;
   }
 `;
-
-const Header = () => {
-  return (
-    <AppHeader>
-      <img src={logo} alt="logo" className="logo" />
-    </AppHeader>
-  );
-};
-
-export default Header;
